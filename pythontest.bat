312df084bf71a1703db6d29bad65d52cd562abb5
Administrator <Administrator@RIALMAIMAN-LAP>
2023-08-20 14:48:55 +0300

    .

--- pythontest.bat
+++ pythontest.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\propy.bat" C:\Users\Administrator\Desktop\pythontest\uiTest.py
+call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\propy.bat" uiTest.py
 pause
 
